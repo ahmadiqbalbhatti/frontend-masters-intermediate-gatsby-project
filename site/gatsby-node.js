29ad57d72ae43133091dd171e64b05b6efbec46a
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-11-09 21:18:31 +0500

    Resolvers are added to extend the BOOK data.

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,8 +1,26 @@
+// const fetch = require('node-fetch');
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+
 const authors = require('./src/data/authors.json');
 const books   = require('./src/data/books.json');
 
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-  const { createNode } = actions;
+  const { createNode, createTypes } = actions;
+
+  // Creating relationship of author node and book node
+  createTypes(`
+    type Author implements Node{
+      books: [Book!]! @link(from: "slug" by: "author.slug")
+    }
+    
+    
+    type Book implements Node{
+      author: Author! @link(from: "author" by: "slug")
+    }
+  `);
+
+
+  // Creating a Node of AUTHOR
   authors.forEach((author) => {
     createNode({
       ...author,
@@ -16,16 +34,32 @@ exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
       },
     });
   });
-  const node = {
-    id      : createNodeId(`some-unique-string`),
-    parent  : null,
-    children: [],
-    internal: {
-      type         : 'MyCustomType',
-      content      : JSON.stringify(nodeContent),
-      contentDigest: createContentDigest(nodeContent),
-    },
-  };
+
+  // Creating a Node of BOOK
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id      : createNodeId(`book-${book.isbn}`),
+      parent  : null,
+      children: [],
+      internal: {
+        type         : 'Book',
+        content      : JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+
+  // const node = {
+  //   id      : createNodeId(`some-unique-string`),
+  //   parent  : null,
+  //   children: [],
+  //   internal: {
+  //     type         : 'MyCustomType',
+  //     content      : JSON.stringify(nodeContent),
+  //     contentDigest: createContentDigest(nodeContent),
+  //   },
+  // };
 
 };
 
@@ -43,4 +77,52 @@ exports.createPages = ({ actions }) => {
       },
     },
   });
+};
+
+exports.createResolvers = ({
+                             actions,
+                             cache,
+                             createNodeId,
+                             createResolvers,
+                             store,
+                             reporter,
+                           }) => {
+  const { createNode } = actions;
+
+
+  const resolvers = {
+
+    Book: {
+      buyLink : {
+        type   : 'String',
+        resolve: (source) => `https://www.powells.com/searchresults?keyword=${source.isbn}`,
+      }, cover: {
+        type: 'File', resolve: async (source) => {
+          const response = await fetch(`https://openlibrary.org/isbn/${source?.isbn}.json`);
+
+          if (!response.ok) {
+            reporter.warn(`Error loading details about ${source.name} - got ${response.status} ${response.statusText}`);
+            return null;
+          }
+
+          const { covers } = await response.json();
+          console.log(covers);
+          if (covers.length) {
+            return createRemoteFileNode({
+              url: `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg`,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+              reporter,
+            });
+          } else {
+            return null;
+          }
+        },
+      },
+    },
+  };
+
+  createResolvers(resolvers);
 };
